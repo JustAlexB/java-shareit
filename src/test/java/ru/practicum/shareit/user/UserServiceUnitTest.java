f2b4ec592f05b0df91b0131d493429f5863d871e
JustAlexB <next.ba@yandex.ru>
2023-04-25 15:48:16 +0300

    fix: style checked

--- src/test/java/ru/practicum/shareit/user/UserServiceUnitTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceUnitTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.practicum.shareit.exceptions.IncorrectParameterException;
 import ru.practicum.shareit.storage.Storage;
