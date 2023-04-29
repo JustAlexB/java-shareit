82a08bc1620741ffbcba50be276a5e147ab80a3d
JustAlexB <next.ba@yandex.ru>
2023-04-29 19:27:35 +0300

    fix: style

--- src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.repository.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
